bbc052bc860ea07ba6b9f0763daa9ab4cf6bc76d
1913198 <1913198@MI034-A33>
2022-03-21 18:20:10 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryAppPrototype
+{
+    public partial class Form1 : Form
+    {
+        public bool listBoxActive(string box)
+        {
+            return false;
+
+        }
+
+        private bool 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
